1e61a6ba4495db3877edae6665b2d301a9334efe
xingy29 <xingy29@mail2.sysu.edu.cn>
2020-12-11 12:51:08 +0800

    new

--- Metagenome/evaluate_SCGs.sh
+++ Metagenome/evaluate_SCGs.sh
@@ -1,2 +1,5 @@
 ../CONCOCT/scripts/COG_table.py -b Annotate/contigs_gt1000_c10K.faa.out -m ../CONCOCT/scgs/scg_cogs_min0.97_max1.03_unique_genera.txt -c concoct_output/clustering_gt1000.csv --cdd_cog_file ../CONCOCT/scgs/cdd_to_cog.tsv > clustering_gt1000_scg.tsv
 ../CONCOCT/scripts/COGPlot.R -s clustering_gt1000_scg.tsv -o clustering_gt1000_scg.pdf
+
+tr "\t" "," < Concoct/coverage_table.tsv > Concoct/coverage_table.csv
+python ../metag-rev-sup/ClusterMeanCov.py Concoct/coverage_table.csv concoct_output/clustering_gt1000.csv Concoct/contigs_10K.fa > cluster_freq.csv
--- scrips/SCGs annotate.sh
+++ scrips/SCGs annotate.sh
@@ -54,3 +54,7 @@ echo "#############   rpsblast took $((EndTime - StartTime)) seconds"
 #需要退出conda环境，conda deactivate
 $CONCOCT/scripts/COG_table.py -b Annotate/contigs_gt1000_c10K.faa.out -m ../CONCOCT/scgs/scg_cogs_min0.97_max1.03_unique_genera.txt -c concoct_output/clustering_gt1000.csv --cdd_cog_file ../CONCOCT/scgs/cdd_to_cog.tsv > clustering_gt1000_scg.tsv
 $CONCOCT/scripts/COGPlot.R -s clustering_gt1000_scg.tsv -o clustering_gt1000_scg.pdf
+
+tr "\t" "," < Concoct/coverage_table.tsv > Concoct/coverage_table.csv
+python ../metag-rev-sup/ClusterMeanCov.py Concoct/coverage_table.csv concoct_output/clustering_gt1000.csv Concoct/contigs_10K.fa > cluster_freq.csv
+
